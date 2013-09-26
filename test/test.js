3c2d5aeb6f99dcc46cc89f8ba22168b11df70e81
Jan Pieter Waagmeester <jieter@jieter.nl>
2013-09-26 10:58:26 +0200

    jshint

--- test/test.js
+++ test/test.js
@@ -1,96 +1,98 @@
-'use strict';
+/* globals mocha:true, describe:true, it:true, expect:true, beforeEach:true, afterEach:true */
+(function () {
+	'use strict';
 
-mocha.setup({
-	ui: 'bdd',
-	reporter: 'html',
-	globals: ['L', 'console', '_leaflet_resize3'],
-	timeout: 5000
-});
+	mocha.setup({
+		ui: 'bdd',
+		reporter: 'html',
+		globals: ['L', 'console', '_leaflet_resize3'],
+		timeout: 5000
+	});
 
-function ignore(providerName) {
-	return providerName.indexOf('MapBox') !== -1 ||
-	       providerName.indexOf('CloudMade') !== -1;
-}
-
-function isOverlay(providerName) {
-	return providerName.match(/^(OpenWeatherMap|OpenSeaMap)/) !== null;
-}
-
-function allLayers() {
-	var layers = {
-		base: [],
-		overlay: []
-	};
-
-	function addLayer(name) {
-		if (ignore(name)) {
-			return;
-		}
-		var type = isOverlay(name) ? 'overlay' : 'base';
-
-		var layer = L.tileLayer.provider(name);
-		layer._name = name;
-
-		layers[type].push(layer);
-	};
-
-	for (var provider in L.TileLayer.Provider.providers) {
-		if (L.TileLayer.Provider.providers[provider].variants) {
-			for (var variant in L.TileLayer.Provider.providers[provider].variants) {
-				addLayer(provider + '.' + variant);
-			}
-		} else {
-			addLayer(provider);
-		}
+	function ignored(providerName) {
+		return providerName.indexOf('MapBox') !== -1 ||
+		       providerName.indexOf('CloudMade') !== -1;
 	}
 
-	return layers;
-}
+	function isOverlay(providerName) {
+		return providerName.match(/^(OpenWeatherMap|OpenSeaMap)/) !== null;
+	}
 
-describe('leaflet-providers', function () {
+	function allLayers() {
+		var layers = {
+			base: [],
+			overlay: []
+		};
 
-	describe('provider definition', function () {
-		describe('when calling with a non-existing provider name', function () {
-			it('throws an error', function () {
-				expect(function () {
-					L.tileLayer.provider('ThisWill.NeverExist')
-				}).to.throwError();
+		function addLayer(name) {
+			if (ignored(name)) {
+				return;
+			}
+			var type = isOverlay(name) ? 'overlay' : 'base';
+
+			var layer = L.tileLayer.provider(name);
+			layer._name = name;
+
+			layers[type].push(layer);
+		}
+
+		for (var provider in L.TileLayer.Provider.providers) {
+			if (L.TileLayer.Provider.providers[provider].variants) {
+				for (var variant in L.TileLayer.Provider.providers[provider].variants) {
+					addLayer(provider + '.' + variant);
+				}
+			} else {
+				addLayer(provider);
+			}
+		}
+
+		return layers;
+	}
+
+	describe('leaflet-providers', function () {
+
+		describe('provider definition', function () {
+			describe('when calling with a non-existing provider name', function () {
+				it('throws an error', function () {
+					expect(function () {
+						L.tileLayer.provider('ThisWill.NeverExist');
+					}).to.throwError();
+				});
 			});
-		});
 
-		var map;
-		beforeEach(function () {
-			map = L.map('map', {
-				attributionControl: false
-			}).setView([0, 0], 5);
-		});
-		afterEach(function () {
-			map.remove();
-		});
-		var layers = allLayers();
+			var map;
+			beforeEach(function () {
+				map = L.map('map', {
+					attributionControl: false
+				}).setView([0, 0], 5);
+			});
+			afterEach(function () {
+				map.remove();
+			});
 
+			var layers = allLayers();
+			['base', 'overlay'].forEach(function (type) {
+				describe(type + ' layers', function () {
+					layers[type].forEach(function (layer) {
+						var minZoom = Math.max(layer.options.minZoom, 1);
 
-		['base', 'overlay'].forEach(function (type) {
-			describe(type + ' layers', function () {
-				layers[type].forEach(function (layer) {
-					var minZoom = layer.options.minZoom;
+						it(layer._name + ' serves a tiles at [0, 0] zoom: ' + minZoom, function (done) {
+							layer.on({
+								tileerror: function (event) {
+									expect().fail('Error loading tile url: ' + event.url);
+								},
+								load: function () {
+									done();
+								}
+							});
 
-					it(layer._name + ' serves a tiles at [0, 0] zoom: ' + minZoom, function (done) {
-						map.setZoom(minZoom);
-
-						layer.addTo(map);
-						layer.on({
-							tileerror: function (event) {
-								expect().fail('Error loading tile url: ' + event.url)
-							},
-							load: function () {
-								done();
-							}
+							map.setZoom(minZoom);
+							layer.addTo(map);
 						});
 					});
 				});
 			});
+		});
 
-		})
 	});
-});
+})();
