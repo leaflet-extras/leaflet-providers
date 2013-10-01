7a47bf8c21858f39f02798f57aa5061bcb254122
Jan Pieter Waagmeester <jieter@jieter.nl>
2013-10-01 16:30:50 +0200

    jshint Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,22 @@
-module.exports = function(grunt) {
+/* jshint node:true */
+module.exports = function (grunt) {
+	'use strict';
+
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		jshint:{
-			options:{
-				jshintrc:".jshintrc"
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc'
 			},
-			all:['leaflet-providers.js']
+			all: ['leaflet-providers.js']
 		},
-		mocha_phantomjs: {
+		'mocha_phantomjs': {
 			all: {
 				options: {
-					urls: [//my ide requries process.env.IP and PORT
-						"http://"+(process.env.IP||"127.0.0.1")+":"+(process.env.PORT||"8080")+"/test/index.html"
+					urls: [ //my ide requries process.env.IP and PORT
+						'http://' + (process.env.IP || '127.0.0.1') +
+						':' + (process.env.PORT || '8080') +
+						'/test/index.html'
 					]
 				}
 			}
@@ -19,7 +24,7 @@ module.exports = function(grunt) {
 		connect: {
 			server: {
 				options: {
-					port: process.env.PORT||8080,
+					port: process.env.PORT || 8080,
 					base: '.'
 				}
 			}
@@ -28,5 +33,5 @@ module.exports = function(grunt) {
 	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 	// Default task(s).
 	grunt.registerTask('test', ['connect', 'mocha_phantomjs']);
-	grunt.registerTask('default', ['jshint','test']);
+	grunt.registerTask('default', ['jshint', 'test']);
 };
