c43aee1b2dda1bbe14a34d251a41af1f5d73bd1a
b_b <brunobergot@gmail.com>
2015-04-05 14:38:58 +0200

    Add OpenTopoMap layer
    New tiles from http://opentopomap.org with credits specified in http://opentopomap.org/about#verwendung

--- leaflet-providers.js
+++ leaflet-providers.js
@@ -100,6 +100,12 @@
 				attribution: 'Map data: &copy; <a href="http://www.openseamap.org">OpenSeaMap</a> contributors'
 			}
 		},
+		OpenTopoMap: {
+			url: '//{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
+			options: {
+				attribution: 'Map data: &copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+			}
+		},
 		Thunderforest: {
 			url: '//{s}.tile.thunderforest.com/{variant}/{z}/{x}/{y}.png',
 			options: {
