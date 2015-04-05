874ccced1ba5f11efe607eb4e23c1c90eb466bc1
b_b <brunobergot@gmail.com>
2015-04-05 15:01:56 +0200

    set maxzoom on opentopomap layer

--- leaflet-providers.js
+++ leaflet-providers.js
@@ -103,6 +103,7 @@
 		OpenTopoMap: {
 			url: '//{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
 			options: {
+				maxZoom: 16,
 				attribution: 'Map data: &copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 			}
 		},
