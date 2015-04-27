35cfa05396683ec5a9623681bb6aa4671c5dbb08
Thomas Rupprecht <rupprecht.thomas+git@gmail.com>
2015-04-27 13:39:05 +0200

    OSM Mapnik and HOT maxZoom 19

--- leaflet-providers.js
+++ leaflet-providers.js
@@ -75,16 +75,23 @@
 		OpenStreetMap: {
 			url: '//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
 			options: {
+				maxZoom: 19,
 				attribution:
 					'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 			},
 			variants: {
 				Mapnik: {},
 				BlackAndWhite: {
-					url: 'http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png'
+					url: 'http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png',
+					options: {
+						maxZoom: 18
+					}
 				},
 				DE: {
-					url: 'http://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png'
+					url: 'http://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png',
+					options: {
+						maxZoom: 18
+					}
 				},
 				HOT: {
 					url: 'http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
