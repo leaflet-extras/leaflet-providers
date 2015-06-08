2db2fa6f3bb1ef1739862123e3ad93ec7579fbc8
dpix <dan.j.pick@gmail.com>
2015-06-08 21:50:12 +1200

    use strict more strictly

--- leaflet-providers.js
+++ leaflet-providers.js
@@ -1,5 +1,3 @@
-'use strict';
-
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
@@ -9,7 +7,7 @@
         factory(L);
     }
 }(this, function (L) {
-
+	'use strict';
 
 	L.TileLayer.Provider = L.TileLayer.extend({
 		initialize: function (arg, options) {
