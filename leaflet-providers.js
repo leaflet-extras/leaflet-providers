14b501f96148963a837997620a9f0a8efbc81dd6
Jan Pieter Waagmeester <jieter@jieter.nl>
2015-09-18 19:00:15 +0200

    Convert spaces to tabs

--- leaflet-providers.js
+++ leaflet-providers.js
@@ -1,14 +1,14 @@
 (function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
-        define(['leaflet'], factory);
-    } else if (typeof modules === 'object' && module.exports) {
-        // define a Common JS module that relies on 'leaflet'
-        module.exports = factory(require('leaflet'));
-    } else {
-        // Assume Leaflet is loaded into global object L already
-        factory(L);
-    }
+	if (typeof define === 'function' && define.amd) {
+		// AMD. Register as an anonymous module.
+		define(['leaflet'], factory);
+	} else if (typeof modules === 'object' && module.exports) {
+		// define a Common JS module that relies on 'leaflet'
+		module.exports = factory(require('leaflet'));
+	} else {
+		// Assume Leaflet is loaded into global object L already
+		factory(L);
+	}
 }(this, function (L) {
 	'use strict';
 
