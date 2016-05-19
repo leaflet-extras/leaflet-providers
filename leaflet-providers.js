4595c2b7d6035163fe38a9a14a97fada38838ffa
Jan Pieter Waagmeester <jieter@jieter.nl>
2016-05-19 20:08:13 +0200

    Set maxZoom for OpenTopoMap to 17, fixes #212

--- leaflet-providers.js
+++ leaflet-providers.js
@@ -138,7 +138,7 @@
 		OpenTopoMap: {
 			url: '//{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
 			options: {
-				maxZoom: 16,
+				maxZoom: 17,
 				attribution: 'Map data: {attribution.OpenStreetMap}, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 			}
 		},
