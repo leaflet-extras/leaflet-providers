1080606427222d295d96fb33533a8272b2847421
JLuc <jluc@no-log.org>
2018-06-20 14:00:13 +0200

    stamen watercolor doesnt provide retina tiles
    https://github.com/stamen/maps.stamen.com/issues/56

--- leaflet-providers.js
+++ leaflet-providers.js
@@ -280,6 +280,7 @@
 				TonerLabels: 'toner-labels',
 				TonerLite: 'toner-lite',
 				Watercolor: {
+					url: 'https://stamen-tiles-{s}.a.ssl.fastly.net/{variant}/{z}/{x}/{y}.{ext}', 
 					options: {
 						variant: 'watercolor',
 						minZoom: 1,
