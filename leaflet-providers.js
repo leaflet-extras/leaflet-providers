4c4c00cc14a3cef3172eb02d4276000fd2359db6
Roman Zoller <rzoller@users.noreply.github.com>
2019-02-03 15:46:08 +0100

    Improve OSM attribution
    - Use "(c) OpenStreetMap contributors" instead of "(c) OpenStreetMap"
      as suggested on https://www.openstreetmap.org/copyright
    - Use https URL

--- leaflet-providers.js
+++ leaflet-providers.js
@@ -81,7 +81,7 @@
 			options: {
 				maxZoom: 19,
 				attribution:
-					'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+					'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 			},
 			variants: {
 				Mapnik: {},
