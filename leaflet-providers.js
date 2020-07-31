eb49e994d4101fd9fb770cb0b2af377735e2f1ab
Christian Quest <cquest@openstreetmap.fr>
2020-07-31 10:26:11 +0200

    New (non dev) URLs for CyclOSM tiles

--- leaflet-providers.js
+++ leaflet-providers.js
@@ -219,7 +219,7 @@
 			}
 		},
 		CyclOSM: {
-			url: 'https://dev.{s}.tile.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png',
+			url: 'https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png',
 			options: {
 				maxZoom: 20,
 				attribution: '<a href="https://github.com/cyclosm/cyclosm-cartocss-style/releases" title="CyclOSM - Open Bicycle render">CyclOSM</a> | Map data: {attribution.OpenStreetMap}'
